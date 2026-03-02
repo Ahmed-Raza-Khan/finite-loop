31bbf272c0af79044c9faab5d37a5afe5314e354
Ahmed Raza <arahmed212214@gmail.com>
2026-03-02 08:28:45 +0500

    V.26.24 - Ahmed Raza - 2/3/26  8:28 Am

--- assests/js/aboutus/milestone-card.js
+++ assests/js/aboutus/milestone-card.js
@@ -29,21 +29,21 @@ const milestoneData = [
 
 if (milestoneContainer) {
     milestoneContainer.innerHTML = milestoneData.map(item => `
-        <div class="relative flex items-center mb-16 last:mb-0 lg:-left-[-10px]">
-            <div class="absolute -left-[36px] lg:-left-[70px] lg:-top-[0px] z-10">
-                <div class="w-12 h-12 lg:w-16 lg:h-16 rounded-full bg-[#5CFFC5] border-[6px] border-[#10B981] flex items-center justify-center shadow-[0_0_15px_rgba(16,185,129,0.4)]">
-                    <span class="text-[#27282A] font-medium text-lg lg:text-xl">${item.step}</span>
+        <div class="relative flex items-center mb-12 lg:mb-16 last:mb-0">
+            <div class="absolute -left-[45px] lg:-left-[61px] z-10 flex items-center justify-center">
+                <div class="w-10 h-10 lg:w-16 lg:h-16 rounded-full bg-[#5CFFC5] border-[4px] lg:border-[6px] border-[#10B981] flex items-center justify-center shadow-[0_0_15px_rgba(16,185,129,0.4)]">
+                    <span class="text-[#27282A] font-bold text-sm lg:text-xl">${item.step}</span>
                 </div>
             </div>
 
-            <div class="bg-none rounded-3xl p-6 lg:p-8 ml-10 w-full transition-all hover:shadow-2xl hover:bg-[#5CFEC533] hover:-translate-y-1">
-                <div class="flex items-start gap-5">
-                    <div class="w-12 h-12 rounded-xl flex items-center justify-center flex-shrink-0">
+            <div class="bg-none rounded-3xl p-5 lg:p-8 w-full transition-all hover:shadow-2xl hover:bg-[#5CFFC51A] hover:-translate-y-1 border border-transparent hover:border-[#5CFFC533]">
+                <div class="flex items-start lg:items-center gap-4 lg:gap-6">
+                    <div class="w-10 h-10 flex-shrink-0">
                         <img src="${item.icon}" alt="icon" class="w-15 h-15">
                     </div>
                     <div>
-                        <h3 class="text-xl text-white lg:text-2xl font-bold mb-2">${item.title}</h3>
-                        <p class="text-[#EFF1F9] leading-relaxed text-sm lg:text-base font-light">
+                        <h3 class="text-lg text-white lg:text-2xl font-bold mb-1 lg:mb-2">${item.title}</h3>
+                        <p class="text-[#EFF1F9] leading-relaxed text-xs lg:text-base font-light">
                             ${item.description}
                         </p>
                     </div>
